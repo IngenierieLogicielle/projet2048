844deed582a95d7d0af6e48af46afb64b469e792
Somnir <matthieu.burtin.somnir@gmail.com>
2015-04-03 11:13:19 +0200

    Ajout de prototypes de fonctions

--- Main/src/main/Case.java
+++ Main/src/main/Case.java
@@ -47,64 +47,8 @@ public class Case
         return true;      
     }
     //fonction pour le mouvement 
-    public boolean move (Case c){
+    public boolean move3LeRetour (Case c){
         
         return true;    
     }
-    
-}
-package main;
-import java.lang.Math.*;
-
-public class Case
-{
-    private int x;
-    private int y;
-    private int value;
-  
-    
-    public Case(){
-        this.x = x;
-        this.y = y;
-        this.value = value;
-    }
-    
-    // modif Joseph
-    
-    // getters
-    public int getX(){
-        return this.x;
-    }
-     public int getY(){
-        return this.y;
-    }
-      public int getValue(){
-        return this.value;
-    }
-      
-    //setters
-      
-    public void setX(int x){
-        this.x = x;
-    }
-    
-    public void setY(int y){
-        this.y = y;
-    }
-    
-    public void setValue(int value){
-        this.value = value;
-    }
-      
-    //détection de la case suivante
-    public boolean getNextCase(int direction){
-        
-        return true;      
-    }
-    
-    public boolean move2 (Case c){
-        
-        return true;    
-    }
-    
 }
