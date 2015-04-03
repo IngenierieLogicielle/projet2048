05321d838abef2795c7da3465af018a86d2287e4
Somnir <matthieu.burtin.somnir@gmail.com>
2015-04-03 10:24:21 +0200

    Reboot projet

--- Main/src/main/Case.java
+++ Main/src/main/Case.java
@@ -3,40 +3,40 @@ import java.lang.Math.*;
 
 public class Case
 {
-	private int value;
-	
-	public Case()
-	{
-		value = 0;
-	}
-	
-	public Case(int v)
-	{
-		value = v;
-	}
-	
-	public Case(boolean r)
-	{
-		if (r)
-		{
-			if (((int)(Math.random()*10) % 2) == 0)
-				value = 2;
-			else
-				value = 4;
-		}
-		else
-		{
-			value = 0;
-		}
-	}
-	
-	public void setValue(int v)
-	{
-		value = v;
-	}
-	
-	public int getValue()
-	{
-		return value;
-	}
+    private int x;
+    private int y;
+    private int value;
+    
+    public Case(){
+        this.x = x;
+        this.y = y;
+        this.value = value;
+    }
+    
+    // getters
+    public int getX(){
+        return this.x;
+    }
+     public int getY(){
+        return this.y;
+    }
+      public int getValue(){
+        return this.value;
+    }
+      
+    //setters
+      
+    public void setX(int x){
+        this.x = x;
+    }
+    
+    public void setY(int y){
+        this.y = y;
+    }
+    
+    public void setValue(int value){
+        this.value = value;
+    }
+      
+      
 }
